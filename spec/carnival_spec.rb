b73e3c862d07c75c424cd57892c09422e342ea73
Reed Hillmar <reedhillmar@gmail.com>
2023-07-31 11:27:56 -0600

    test: test code for most popular ride

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -32,4 +32,20 @@ describe Carnival do
       expect(@carnival1.rides).to eq([@ride1, @ride2, @ride3])
     end
   end
+
+  describe "#most_popular_ride" do
+    it "can return the most popular ride" do
+      @carnival1.add_ride(@ride1)
+      @carnival1.add_ride(@ride2)
+      @carnival1.add_ride(@ride3)
+      @ride1.board_rider(@visitor1)
+      @ride1.board_rider(@visitor2)
+      @ride1.board_rider(@visitor1)
+      @ride3.board_rider(@visitor1)
+      @ride3.board_rider(@visitor2)
+      @ride3.board_rider(@visitor3)
+
+      expect(@carnival1.most_popular_ride).to eq(@ride1)
+    end
+  end
 end
