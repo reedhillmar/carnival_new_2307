d18ef569521feb725931b96622679c6a2036eed9
Reed Hillmar <reedhillmar@gmail.com>
2023-07-31 11:20:32 -0600

    test: add test code for Carnival.initialize

---
+++ spec/carnival_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe Carnival do
+  before(:each) do
+    @carnival1 = Carnival.new({duration: 14, rides: []})
+    @ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+    @ride2 = Ride.new({ name: 'Ferris Wheel', min_height: 36, admission_fee: 5, excitement: :gentle })
+    @ride3 = Ride.new({ name: 'Roller Coaster', min_height: 54, admission_fee: 2, excitement: :thrilling })
+    @visitor1 = Visitor.new('Bruce', 54, '$10')
+    @visitor2 = Visitor.new('Tucker', 36, '$5')
+    @visitor3 = Visitor.new('Penny', 64, '$15')
+    @visitor1.add_preference(:gentle)
+    @visitor2.add_preference(:gentle)
+    @visitor2.add_preference(:thrilling)
+    @visitor3.add_preference(:thrilling)
+  end
+
+  describe "#initialize" do
+    it "can exist and have details" do
+      expect(@carnival1).to be_a Carnival
+      expect(@carnival1.duration).to eq(14)
+      expect(@carnival1.rides).to eq ([])
+    end
+  end
+end
