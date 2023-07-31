3f8829a7008a358c8b4170664d6b97b611af00ea
Reed Hillmar <reedhillmar@gmail.com>
2023-07-31 10:58:31 -0600

    test: add test code for fee collection

--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -31,5 +31,15 @@ describe Ride do
 
       expect(@ride1.rider_log).to eq({@visitor1 => 2, @visitor2 => 1})
     end
+
+    it "can charge fees and collect revenue" do
+      @ride1.board_rider(@visitor1)
+      @ride1.board_rider(@visitor2)
+      @ride1.board_rider(@visitor1)
+
+      expect(@visitor1.spending_money).to eq(8)
+      expect(@visitor2.spending_money).to eq(4)
+      expect(@ride1.total_revenue).to eq(3)
+    end
   end
 end
