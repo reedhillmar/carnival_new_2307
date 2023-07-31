30ed89cca78d3257e6224ef13874e5b7709181ab
Reed Hillmar <reedhillmar@gmail.com>
2023-07-31 11:11:43 -0600

    test: add test code for eligible riders

--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -10,6 +10,8 @@ describe Ride do
     @visitor3 = Visitor.new('Penny', 64, '$15')
     @visitor1.add_preference(:gentle)
     @visitor2.add_preference(:gentle)
+    @visitor2.add_preference(:thrilling)
+    @visitor3.add_preference(:thrilling)
   end
 
   describe "#initialize" do
@@ -40,6 +42,21 @@ describe Ride do
       expect(@visitor1.spending_money).to eq(8)
       expect(@visitor2.spending_money).to eq(4)
       expect(@ride1.total_revenue).to eq(3)
+    end
+
+    it "only puts on riders that are eligible" do
+      @ride1.board_rider(@visitor1)
+      @ride1.board_rider(@visitor2)
+      @ride1.board_rider(@visitor1)
+      @ride3.board_rider(@visitor1)
+      @ride3.board_rider(@visitor2)
+      @ride3.board_rider(@visitor3)
+
+      expect(@visitor1.spending_money).to eq(8)
+      expect(@visitor2.spending_money).to eq(4)
+      expect(@visitor3.spending_money).to eq(13)
+      expect(@ride3.rider_log).to eq({@visitor3})
+      expect(@ride3.total_revenue).to eq(2)
     end
   end
 end
