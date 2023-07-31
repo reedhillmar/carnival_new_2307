65a45c7a7f0ce8d16743137187314462c5a0bafb
Reed Hillmar <reedhillmar@gmail.com>
2023-07-31 10:27:54 -0600

    test: add test spec for Visitor.initialize

---
+++ spec/visitor_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe Visitor do
+  before(:each) do
+    @visitor1 = Visitor.new('Bruce', 54, '$10')
+    @visitor2 = Visitor.new('Tucker', 36, '$5')
+    @visitor3 = Visitor.new('Penny', 64, '$15')
+  end
+
+  describe "#initialize" do
+    it "can exist and have details" do
+      expect(@visitor1).to be_a Visitor
+      expect(@visitor1.name).to eq(Bruce)
+      expect(@visitor1.height).to eq(54)
+      expect(@visitor1.spending_money).to eq(10)
+      expect(@visitor1.preferences).to eq([])
+    end
+  end
+end
